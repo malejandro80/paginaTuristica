1ba9973cc4456cbeb3d700d47f558b92f313ff56
miguel <m.alejandro_80@hotmail.com>
2018-08-09 22:22:14 -0400

    se agrego funcionalidad a los botones del navbar

--- docs/js/code.js
+++ docs/js/code.js
@@ -1,3 +1,27 @@
 $(document).ready(function(){
-alert("hola");
+
+	$("#inicio-button").on("click", function(){
+		//$(window).focus("#nosotros");
+		$('html,body').animate({
+        scrollTop: $("#inicio").offset().top
+    }, 1500);
+	})
+	$("#nosotros-button").on("click", function(){
+		//$(window).focus("#nosotros");
+		$('html,body').animate({
+        scrollTop: $("#nosotros").offset().top
+    }, 1500);
+	})
+	$("#paquetes-button").on("click", function(){
+		//$(window).focus("#nosotros");
+		$('html,body').animate({
+        scrollTop: $("#servicios").offset().top
+    }, 1500);
+	})
+	$("#contacto-button").on("click", function(){
+		//$(window).focus("#nosotros");
+		$('html,body').animate({
+        scrollTop: $("#contacto").offset().top
+    }, 1500);
+	})
 });
